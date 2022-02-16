b9d4d6a1511d21d6bc5ecdc45b8c5a5cf771b32d
AnjanaLP <anjana.lpn@gmail.com>
2022-02-16 20:56:38 +0000

    refactor individualFrameScores function

--- src/game.js
+++ src/game.js
@@ -25,15 +25,11 @@ class Game {
       let frame = this._frames[i];
       let sum = 0;
       sum += frame.sumOfPinsKnocked();
-      if (this._nextFrame(frame, 1)) {
+      if ( this._nextFrame(frame, 1)) {
         if (frame.isStrike()) {
-          if (this._nextFrame(frame, 1).isStrike()) {
-            sum += this._nextFrame(frame, 1).bonus(2);
-            if (this._nextFrame(frame, 2)) {
-              sum += this._nextFrame(frame, 2).bonus(1);
-            }
-          } else {
-            sum += this._nextFrame(frame, 1).bonus(2);
+          sum += this._nextFrame(frame, 1).bonus(2);
+          if (this._isTwoConsecutiveStrikes(frame)) {
+            sum += this._nextFrame(frame, 2).bonus(1);
           }
         } else if (frame.isSpare()) {
           sum += this._nextFrame(frame, 1).bonus(1);
@@ -44,8 +40,11 @@ class Game {
     return sums;
   }
 
-  _nextFrame(frame, num) {
-    let frameIndex = this._frames.indexOf(frame);
-    return this._frames[(frameIndex + num)];
+  _isTwoConsecutiveStrikes(frame) {
+    return this._nextFrame(frame, 1).isStrike() && this._nextFrame(frame, 2);
+  }
+
+  _nextFrame(frame, index_from_frame) {
+    return this._frames[this._frames.indexOf(frame) + index_from_frame];
   }
 }
