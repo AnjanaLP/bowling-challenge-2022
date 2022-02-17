9383e03941258f6d1dbb4579d5b4815351d4b01d
AnjanaLP <anjana.lpn@gmail.com>
2022-02-17 12:42:44 +0000

    refactor feature test to use a loop

--- spec/featureTestsSpec.js
+++ spec/featureTestsSpec.js
@@ -6,23 +6,17 @@ describe('Score calculation', () => {
   });
 
   it('a mixed game of strikes and spares returns the correct score', () => {
-    game.createFrame([1, 4])
-    game.createFrame([4, 5])
-    game.createFrame([6, 4])
-    game.createFrame([5, 5])
-    game.createFrame([10, 0])
-    game.createFrame([0, 1])
-    game.createFrame([7, 3])
-    game.createFrame([6, 4])
-    game.createFrame([10, 0])
-    game.createFrame([2, 8, 6])
+    const rolls = [ [1, 4], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 4], [10, 0], [2, 8, 6] ]
 
+    for (let i = 0; i < 10; i++) {
+      game.createFrame(rolls[i]);
+    }
     expect(game.runningTotalScore()).toEqual([ 5, 14, 29, 49, 60, 61, 77, 97, 117, 133]);
   });
 
   it('a gutter game returns the correct score', () => {
     for (let i = 0; i < 10; i++) {
-      game.createFrame([0, 0])
+      game.createFrame([0, 0]);
     }
     expect(game.runningTotalScore()).toEqual([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]);
   });
